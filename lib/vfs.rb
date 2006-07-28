e1a65d84df26a13009346553d61f6d0a44c09da4
Christian Theil Have <christiantheilhave@gmail.com>
2006-07-28 21:08:34 +0000

    ja

--- lib/vfs.rb
+++ lib/vfs.rb
@@ -1,6 +1,8 @@
 require 'set'
 
 module VFS
+    
+    # Convert a object (path) to a str (if you can ;-)
     def VFS.to_path( path )
         path = path.to_str if path.respond_to? :to_str
         if /\0/ =~ path
@@ -9,11 +11,13 @@ module VFS
         return path
     end
     
+    # Starts with / ?
     def VFS.absolute?( path )
         path = VFS.to_path(path)
         %r{\A/} =~ path
     end
     
+    # Removes redundant . and resolves .. (pure str magic)
     def VFS.cleanpath( path )
         path = VFS.to_path(path)
         if !VFS.absolute?(path)
@@ -35,61 +39,100 @@ module VFS
         path << names.join('/')
     end
     
+    # Voodoo
     class OverlayFileObject
-        attr_reader :proxy
+        attr_reader :proxy, :parent, :name
         #delegate :class, :to => :context
         #delegate :is_a?, :to => :context
         
-        def initialize( fs, proxy )
+        def initialize( fs, proxy, name=nil, parent=nil )
             @fs = fs
             @proxy = proxy
+	    @name = VFS.to_path(name).dup if name
+            @parent = parent
         end
+
+	def path( end_slash=false )
+            if @name
+		parentpath + @name + (end_slash ? '/' : '')
+	    else
+		parentpath
+	    end
+	end
+        alias :to_str :path
+        alias :to_s :path
+
+	def parentpath
+	    if @parent
+	    	loc_path = @parent.path
+            	loc_path += '/' unless /\/$/ =~ loc_path
+	    	loc_path
+	    else
+		'/'
+	    end
+	end
         
         def exists?
-            !@fs.entries( @proxy.path ).empty? || @proxy.exists?
+            !@fs.overlayset( self.path ).empty? || @proxy.exists?
         end
         alias :exist? :exists?
         
         def entries
-            overlays = @fs.entries( @proxy.path )
-            overlays.merge( @proxy.entries ) unless overlays.empty?
+            overlays = @fs.overlayset( self.path )
+            overlays.merge( @proxy.entries ) if overlays.empty? || (@proxy.exists? && @proxy.directory?)
             overlays.to_a
         end
         
         def resolve( name )
-            wrap( @proxy.resolve( name ) )
+	    mountpath = self.path(true) + name
+	    if @fs.mount?( mountpath )
+		wrap( @fs.mount( mountpath ), name, self )
+	    else
+            	wrap( @proxy.resolve( name ), name, self )
+	    end
         end
             
-        def wrap( proxy )
-            OverlayFileObject.new( @fs, proxy ) unless proxy.nil?
+        def wrap( proxy, name, parent )
+            OverlayFileObject.new( @fs, proxy, name, parent ) unless proxy.nil?
         end
         
         def method_missing( method, *args, &block )
+	    puts "Ruby is doing magic"
             @proxy.send( method, *args, &block )
         end
         private :method_missing
     end
     
+    #
+    # FileSystem's primary purpose is to maintain the @mounts table
+    # @entries contains the list of all paths and parent paths
+    # mount and unmount does what you think they do ..
     class FileSystem
         def initialize( rootfile )
             @mounts = {'/' => OverlayFileObject.new( self, rootfile )}
-            @entries = {}
+            @overlays = {} # path => set
         end
       
-        def mount( path, rootfile )
+	# mount also adds all parent paths to @entries
+        def mount( path, rootfile=nil )
             path = VFS.cleanpath(path)
-            @mounts[path] = OverlayFileObject.new( self, rootfile )
-            names = []
-            path.scan(%r{[^/]+}) {|name|
-                entryname = '/' + names.join('/')
-                if !@entries[entryname] 
-                    @entries[entryname] = [].to_set
-                end
-                @entries[entryname].add name
+	    if @rootfile.nil?
+		return @mounts[path]
+            end
+	    @mounts[path] = OverlayFileObject.new( self, rootfile )
+	    names = []
+	    path.scan(%r{[^/]+}) {|name|
+                overlay_path = '/' + names.join('/')
+                @overlays[overlay_path] = [].to_set unless @overlays[overlay_path]
+                @overlays[overlay_path].add name
                 puts "#{entryname} => #{name}"
                 names << name
             }
-        end
+	end
+
+	def mount?( path )
+	    @mounts.has_key?(path)
+	end    
       
         def unmount( path )
             path = path.path if path.respond_to? :path
@@ -97,16 +140,17 @@ module VFS
             entries = @mounts.delete( path )
         end
         
-        def entries(path)
-            path = VFS.cleanpath(path);
-            entry = @entries[path]
-            entry ? entry : [].to_set
+        def overlayset(path)
+            path = VFS.cleanpath(path)
+            overlay = @overlays[path]
+            overlay ? overlay : [].to_set
         end
       
+	# Never fails if arguments are valid
         def lookup( path, basepath="/" )
             path = VFS.to_path(path)
-            path = basepath + path unless VFS.absolute?(path)
             if !VFS.absolute?(path)
+            	basepath = basepath.path if basepath.respond_to? :path
                 basepath = VFS.cleanpath( basepath )
                 basepath += "/" unless /\/$/ =~ basepath
                 path = basepath + path
@@ -114,6 +158,10 @@ module VFS
             path = VFS.cleanpath( path )
             
             fileobj = @mounts['/']
+		
+            # tokenize on / and resolve the name.
+	    # We need to do it this way because a fileobject is resposibly for all of the leafs below it.
+            # Only the fileobject knows how to proceed 
             path.scan(%r{[^/]+}) {|name|
                 fileobj = fileobj.resolve(name)
             }
@@ -122,38 +170,27 @@ module VFS
       
     end
     
-    class FSFileObject
-        attr_reader :parent, :name
-
-        def initialize( name, parent, fs_parent=parent )
-            @parent = parent
-            @fs_parent = parent
-            @name = name
-            self.taint if @name.tainted? || @parent.tainted?
-        end
-
-        def path
-            loc_path = (@parent ? @parent.path : "/" )
-            loc_path += '/' unless /\/$/ =~ loc_path
-            loc_path += @name if @name
-            return loc_path 
-        end
-        
-        alias :to_str :path
-        alias :to_s :path
-
+    # Objects of this class corresponds to an entry in the physical fs.
+    # Same with derivatives.
+    class FSBaseFileObject
         def resolve( name )
             FSFileObject.new( name, self )
         end
+    end
+
+    class FSFileObject < FSBaseFileObject
+	def initialize( name, fs_parent )
+	    @name = name
+	    @fs_parent = fs_parent
+	end
 
         def fs_filepath
             (@fs_parent ? @fs_parent.fs_filepath : "" ) + File::SEPARATOR + @name
         end
     end
     
-    class FSFileRoot < FSFileObject
-        def initialize( rootpath, parent=nil )
-            super(nil, parent, nil)
+    class FSFileRoot < FSBaseFileObject
+        def initialize( rootpath )
             @rootpath = VFS.cleanpath( rootpath )
             self.taint if @rootpath.tainted?
         end
@@ -163,6 +200,7 @@ module VFS
         end
     end
 
+    # Also a physical fs object
     class FSFileObjectMeta
         def initialize( fileObject )
             @file = fileObject
@@ -202,8 +240,7 @@ def test( names )
 end
 
 module VFS
-    class FSFileObject
-
+    class FSBaseFileObject
         def directory?() File.directory?( self.fs_filepath ) end
 
         def file?() File.file?( fs_filepath ) end
@@ -217,12 +254,17 @@ module VFS
     end
 end
 
+module TestI
+  include VFS
+end
 
-t = VFS::FileSystem.new( VFS::FSFileRoot.new("/Users/griff/Pictures"))
+t = TestI::FileSystem.new( VFS::FSFileRoot.new("/tmp"))
+#TestI.new
+#t = VFS::FileSystem.new( VFS::FSFileRoot.new("/Users/griff/Pictures"))
 t.mount( '/test/molla', VFS::FSFileRoot.new('/Users/griff/Music') )
 t.mount( '/molla/ff', VFS::FSFileRoot.new('/Users/griff/Music') )
-puts t.entries('/')
-puts t.lookup( "").exists?
+puts t.overlayset('/')
+puts t.lookup( "").directory?
 puts t.lookup( "").entries
 puts "Next"
 puts t.lookup("/heelo/../../../test")
