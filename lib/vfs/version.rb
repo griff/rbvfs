8278404486e647fa4660aa7145b9883347a2d52b
Brian Olsen <brian@maven-group.org>
2008-03-02 23:50:47 +0000

    Added keywords and moved old code.

---
+++ lib/vfs/version.rb
@@ -0,0 +1,15 @@
+# $Id$
+#
+# Version information.
+#
+# Author::    Brian Olsen (mailto:bro@rubyforge.org)
+# Copyright:: Copyright (c) 2007, Brian Olsen
+# License::   VirtualFiles is free software distributed under a BSD style license.
+#             See LICENSE[file:../LICENSE.html] for permissions.
+
+module VFS
+
+  # The version of the VirtualFiles library.
+  VFS_VERSION = '0.1.0'
+  
+end
